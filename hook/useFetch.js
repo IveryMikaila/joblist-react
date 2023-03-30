a62d9bc2893a6ec6a488c77bb33cb3c02fcc1d4c
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-03-30 00:34:01 -0400

    Added Popular Jobs Section to Homepage

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,11 +1,8 @@
 import { useState,useEffect } from "react";
 import axios from "axios";
-import {RAPID_API_KEY} from '@env';
-
-const rapidApiKey = RAPID_API_KEY;
 
 
-const useFetch = (endpoint) =>{
+const useFetch = (endpoint,query) =>{
     const [data, setData] =useState([]);
     const [isLoading, setIsLoading] =useState(false); 
     const [error, setError] =useState(null);  
@@ -14,10 +11,35 @@ const useFetch = (endpoint) =>{
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            'X-RapidAPI-Key':rapidApiKey ,
+            'X-RapidAPI-Key':'427cb98747mshe49402e980f3770p131347jsn036e2aab5164' ,
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
           },
-        params: {query: 'Python developer in Texas, USA', page: '1', num_pages: '1'},
+        params:{...query},
       };
-      
+   
+      const fetchData = async () =>{
+       setIsLoading(true);
+       try {
+        const response = await axios.request(options);
+        setData(response.data.data);
+setIsLoading(false);
+       } catch (error) {
+        setError(error);
+        alert('There is an error')
+       } finally{
+setIsLoading(false)
+       }
+      }
+
+useEffect(() =>{
+    fetchData(); 
+},[]);
+
+const refetch = () =>{
+    setIsLoading(true);
+    fetchData();
 }
+return {data , isLoading, error, refetch};
+
+}
+export default useFetch;
